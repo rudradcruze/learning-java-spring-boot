27239e96fbdaf0802885540294e42a74e2d68960
rudradcruze <francisrudra@gmail.com>
2023-03-01 10:59:07 +0600

    condition update

---
+++ day-4-condition/README.md
@@ -0,0 +1,6 @@
+# Conditional Statements
+* [Simple if statement](./SimpleIf.java) <br>
+* [If else statement](./IfElseStatement.java) <br>
+* [Else if statement](./ElseIfStatement.java) <br>
+* [Ternary operator](./TernaryOperator.java) <br>
+* [Switch case statement](./SwitchCase.java) <br>
---
+++ day-4-condition/SimpleIf.java
@@ -0,0 +1,16 @@
+package ac.dia.condition;
+
+// Simple condition program to get into the if block
+
+public class SimpleIf {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		if(x < 20) {
+			System.out.println("This is if statement.");
+		} // end of if
+
+	} // end of main
+
+} // end
