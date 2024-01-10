6add01902ba87a1d6918646f896949d0d0efdb8d
rudradcruze <francisrudra@gmail.com>
2024-01-10 10:51:14 +0600

    amigo rest api

---
+++ practice-spring-boot/spring-boot-example-project/src/main/java/org/rudradcruze/Main.java
@@ -0,0 +1,169 @@
+package org.rudradcruze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "com.rudradcruze")
+//@EnableAutoConfiguration
+@RestController
+public class Main {
+
+    // db
+    private static List<Customer> customers;
+
+    static {
+        customers = new ArrayList<>();
+        Customer alex = new Customer(
+                1,
+                "Alex",
+                "alex@gmail.com",
+                21
+        );
+        customers.add(alex);
+        Customer jamila = new Customer(
+                2,
+                "Jamila",
+                "jamila@gmail.com",
+                19
+        );
+        customers.add(jamila);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    static class Customer {
+        private Integer id;
+        private String name;
+        private String email;
+        private Integer age;
+
+        private Customer() {}
+
+        public List<Customer> getCustomer() {
+            return customers;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public Customer(Integer id, String name, String email, Integer age) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Customer customer = (Customer) o;
+            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, email, age);
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+
+//    @GetMapping("/greet")
+//    public GreetResponse greet(@RequestParam(value = "name", required = false) String name) {
+//        String greetMessage = name == null || name.isBlank()? "Hello" : "Hello " + name;
+//        GreetResponse response = new GreetResponse(
+//                greetMessage,
+//                List.of("Java", "Golang", "Javascript"),
+//                new Person("Rudra", 21, 30_000));
+//        return response;
+//    }
+//
+//    record Person (String name, int age, double savings) {
+
+//    }
+
+//    record GreetResponse(
+//            String greet,
+//            List<String> facProgrammingLanguages,
+//            Person person) {}
+
+//    class GreetResponse {
+//        private final String greet;
+//
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
+}
